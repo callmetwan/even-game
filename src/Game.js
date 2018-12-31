6cd8d1dc9424c43eb2aad7c5ab0af9a38598f090
Anthony Garritano <anthony@anthonygarritano.com>
2018-12-31 00:02:36 -0500

    Implement RotatingPlatform

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 // @flow
 import Ball from './Ball'
+import RotatingPlatform from './RotatingPlatform'
 
 type GameConfig = {
 	canvas: HTMLCanvasElement,
@@ -9,68 +10,30 @@ type GameConfig = {
 export default class Game {
 	canvas: HTMLElement | null
 	ctx: CanvasRenderingContext2D
+
+	ball: Ball
+	rotatingPlatform: RotatingPlatform
+
 	canvasCenterX: number
 	canvasCenterY: number
-	ball: Ball
-	circleRadius: number
-	circleX: number
-	circleY: number
-	oneDegreeAsRadian: number
-	currentRadian: number
-	numberOfRotations: number
-	ballRadius: number
-	ballX: number
-	ballY: number
-
 
 	constructor(config: GameConfig) {
 		this.canvas = config.canvas
 		this.ctx = this.canvas.getContext('2d')
 		this.canvasCenterX = this.canvas.width / 2
 		this.canvasCenterY = this.canvas.height / 2
-		this.circleRadius = 100
-		this.circleX = this.canvasCenterX
-		this.circleY = this.canvasCenterY + 70
-		this.oneDegreeAsRadian = 0.01745329
-		this.currentRadian = 0
-		this.numberOfRotations = 0
 
+		this.rotatingPlatform = new RotatingPlatform(this.ctx, {
+			xPos: this.canvasCenterX,
+			yPos: this.canvasCenterY + 70,
+			radius: 100,
+		})
+		
 		this.ball = new Ball(this.ctx, {
 			xPos: this.canvasCenterX,
-			yPos: this.circleY - this.circleRadius - 10,
+			yPos: this.rotatingPlatform.yPos - this.rotatingPlatform.radius - 10,
 			radius: 10,
 		})
-	}
-
-
-	drawCircle = (numberOfSections: number) => {
-		const sizeOfLine = (Math.PI * 2) / numberOfSections
-		const sizeOfHole = 0.3141592653589793
-		this.ctx.save()
-		for (let i = 0; i < numberOfSections; i++) {
-			const start = (i * sizeOfLine - this.currentRadian) + sizeOfHole
-			const stop = (i === numberOfSections)
-				? Math.PI * 2 - this.currentRadian
-				: (i + 1) * sizeOfLine - this.currentRadian
-			const color = (i + 1 === numberOfSections) ? 'red' : 'blue'
-			this.drawArc(start, stop, color)
-			this.ctx.stroke()
-			this.ctx.closePath()
-		}
-		this.ctx.restore()
-	}
-
-	drawArc = (start: number, stop: number, color: string = 'blue') => {
-		this.ctx.beginPath()
-		this.ctx.arc(this.circleX, this.circleY, this.circleRadius, start, stop)
-		this.ctx.strokeStyle = color
-	}
-
-	addRadian = (baseRadian: number, numberToAdd: number) => {
-		const combinedRadian = baseRadian + numberToAdd
-		return (baseRadian > 6.283185)
-			? 0
-			: combinedRadian
 	}
 
 	writeScore = (msg: string | number) => {
@@ -81,34 +44,34 @@ export default class Game {
 	}
 
 	ballCircleAreColliding = () => {
-		const a2 = Math.pow(this.ballY - this.circleY, 2)
-		const b2 = Math.pow(this.ballX - this.circleX, 2)
+		const a2 = Math.pow(this.ball.yPos - this.rotatingPlatform.yPos, 2)
+		const b2 = Math.pow(this.ball.xPos - this.rotatingPlatform.xPos, 2)
 		const c2 = a2 + b2
 		const distance = Math.sqrt(c2)
 
-		return distance < ((this.ballRadius + this.circleRadius))
+		return distance < ((this.ball.radius + this.rotatingPlatform.radius))
 	}
 
 	render = () => {
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-		const sectionsForCircle = this.numberOfRotations + 2
+		const sectionsForCircle = this.rotatingPlatform.numberOfRotations + 2
 
-		this.drawCircle(sectionsForCircle < 10 ? sectionsForCircle : 10)
+		this.rotatingPlatform.render(sectionsForCircle)
 		this.ball.render()
 
 		if (this.ballCircleAreColliding()) {
-			let message = ''
-			const imageData = this.ctx.getImageData(this.circleX, this.circleY - this.circleRadius, 1, 1).data
+			const imageData = this.ctx.getImageData(
+				this.rotatingPlatform.xPos,
+				this.rotatingPlatform.yPos - this.rotatingPlatform.radius,
+				1,
+				1).data
 			if (imageData.every(element => !element)) {
 				this.writeScore(':(')
 			} else {
-				this.writeScore(this.numberOfRotations)
+				this.writeScore(this.rotatingPlatform.numberOfRotations)
 			}
 
 		}
-
-		this.currentRadian = this.addRadian(this.currentRadian, this.oneDegreeAsRadian)
-		this.numberOfRotations = this.currentRadian === 0 ? this.numberOfRotations + 1 : this.numberOfRotations
 
 		window.requestAnimationFrame(this.render)
 	}
